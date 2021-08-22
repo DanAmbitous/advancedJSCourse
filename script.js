63ec4501e8a9636e5ef63a92c69a5c4732b54d17
Danial A <danazad2016@gmail.com>
2021-08-22 13:15:50 -0500

    Initial Upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const items = [
+  {
+    id: 1,
+    name: "test1",
+    description: "desc1",
+  },
+  {
+    id: 2,
+    name: "test2",
+    description: "desc2",
+  },
+  {
+    id: 3,
+    name: "test3",
+    description: "desc3",
+  },
+]
+
+const map1 = new Map([])
+
+map1.set(1, {
+  id: 1,
+  name: "test1",
+  description: "desc1",
+})
+map1.set(2, {
+  id: 2,
+  name: "test2",
+  description: "desc2",
+})
+map1.set(3, {
+  id: 3,
+  name: "test3",
+  description: "desc3",
+})
