82a5311a53016c9277cd80d1f33bd47d082fce04
Danial A <danazad2016@gmail.com>
2021-08-22 15:53:44 -0500

    Doing the aforementioned thing but with set

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 function removeDups(array) {
-  let tracker = {}
-
-  console.log(array)
-
-  array.forEach((element) => {
-    tracker[element] = element
-  })
-
-  console.log(tracker)
-
-  array.length = ""
-
-  for (const [key, value] of Object.entries(tracker)) {
-    array.push(Number(key))
-  }
-
-  console.log(array)
+  return [...new Set(array)]
 }
 
 removeDups([1, 2, 3, 4, 3, 2, 5])
