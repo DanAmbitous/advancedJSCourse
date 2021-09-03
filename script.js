abbf04ab93d75fb63a47c9d99886ecf61e1033a4
Danial A <danazad2016@gmail.com>
2021-09-02 19:21:54 -0500

    Went a bit further into Getters and Setters

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const person = {
   _username: "Player 80100",
+  _password: Math.floor(Math.random() * 100000),
   get username() {
     return this._username
   },
   set username(value) {
     if (value.length >= 5) this._username = value
   },
+  get password() {
+    return this._password
+  },
+  set password(password) {
+    if (password.length >= 5) this._password = password
+  },
 }
 
 person.username = "DanAstonshing"
+person.password = "123238"
 
-console.log(person.username)
+console.log(person.username, person.password)
